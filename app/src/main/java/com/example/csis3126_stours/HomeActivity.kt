77a3006201e387846db3b3f4b41d536852522b4b
Kyle <Kyle@DESKTOP-BMM598G>
2022-10-02 20:30:53 -0400

    Account Authentication through firebase added

---
+++ app/src/main/java/com/example/csis3126_stours/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.csis3126_stours
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
